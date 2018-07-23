fb289bfa6b6a8f712253dca3fe093d8e5d9d517c
wentaojia2014 <wentaojia2014@163.com>
2018-07-23 22:15:10 +0800

    713

---
+++ Array/713.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        
+        int ans = 0;
+        int n = nums.size();
+        int product = 1.0;
+        int left = 0;
+        for (int i = 0; i < n; i++) {
+            product *= nums[i];
+            while (product >= k) {
+                product /= nums[left];
+                left++;   
+            }
+            ans += i - left + 1;
+
+        }
+        return ans;
+    }
+
+};
+int main() {
+    vector<int> v{10, 5,2,6};
+
+    Solution s;
+    cout << s.numSubarrayProductLessThanK(v, 100) << endl;
+
+    return 0;
+}
